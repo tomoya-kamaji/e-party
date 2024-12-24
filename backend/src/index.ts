95f2953debcd0d28bfd45b4403d784970c66d752
tomoya-kamaji <tomoya.kamaji@gmail.com>
2024-12-24 15:42:55 +0900

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,16 @@
-import { serve } from '@hono/node-server'; // サーバー起動用モジュールをインポート
+import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import pokerApp from './feature/poker';
 
-// const app = new Hono().basePath('/api');
-
-const app = new Hono().route('/pokers', pokerApp).route('/authors', pokerApp);
+const app = new Hono().basePath('/api').route('/pokers', pokerApp);
 
 // サーバー起動
-serve(app);
+const port = 3008;
+serve({
+  fetch: app.fetch,
+  port,
+});
+console.log(`Server is running on ${port}`);
 
 export type AppType = typeof app;
 
--- client/src/feature/healthCheck/api/indext.ts
+++
@@ -1,7 +0,0 @@
-// authors.ts
-import { Hono } from 'hono';
-
-const app = new Hono()
-  .get('/', (c) => c.json('list authors'))
-  .post('/', (c) => c.json('create an author', 201))
-  .get('/:id', (c) => c.json(`get ${c.req.param('id')}`));
