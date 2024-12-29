9d621b3680c51ef2f9725e892ecaa01ce67aa149
tomoya-kamaji <tomoya.kamaji@gmail.com>
2024-12-29 12:51:52 +0900

    型定義

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
 import { useAuth } from '@/state/AuthContext';
-import { honoClient } from '@/util/api';
 
 export const Home = async () => {
   const { session } = useAuth();
-  const res = await honoClient.api.pokers.$get();
-  console.log(res);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
