3de12f22ccc579fe5bc73b60ac74068522872c53
tomoya-kamaji <tomoya.kamaji@gmail.com>
2025-01-25 12:11:30 +0900

    Api Route仮実装

--- client/src/util/api/client.ts
+++ client/src/util/api/client.ts
@@ -4,6 +4,7 @@ import { hc } from 'hono/client';
 import { HonoAppType } from '../../../../backend/src';
 import { getAuthToken } from '../supabase/client';
 import { getEndpointsConfig } from '../env';
+import { NextAppType } from '@/app/api/[[...route]]/route';
 
 /**
  * Honoのクライアント
@@ -29,3 +30,31 @@ export const honoClient = hc<HonoAppType>(getEndpointsConfig().backendUrl, {
     }
   },
 });
+
+export const nextClient = hc<NextAppType>('http://localhost:3005', {
+  fetch: async (input: RequestInfo | URL, init: RequestInit | undefined) => {
+    const token = await getAuthToken();
+
+    // ヘッダーを設定
+    const headers = new Headers(init?.headers);
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    try {
+      const res = await fetch(input, {
+        ...init,
+        headers,
+      });
+      return res;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+});
+
+export const fetchCurrentUser = async () => {
+  const res = await nextClient.api.users.current.$get();
+  const json = await res.json();
+  return json;
+};
