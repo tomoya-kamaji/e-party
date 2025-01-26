5016503ad234d03ac87b13f9f80e4e47f9ac9516
tomoya-kamaji <tomoya.kamaji@gmail.com>
2025-01-26 10:36:44 +0900

    useCase

--- client/src/app/api/[[...route]]/route.ts
+++ client/src/app/api/[[...route]]/route.ts
@@ -1,9 +1,16 @@
+import roomApp from '@/server/feature/room';
 import userApp from '@/server/feature/user';
 import { authMiddleware } from '@/server/middleware';
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 
-const app = new Hono().basePath('/api').use(authMiddleware).route('/users', userApp)
+const app = new Hono()
+  .basePath('/api')
+  .use(authMiddleware)
+  // user
+  .route('/users', userApp)
+  // room
+  .route('/rooms', roomApp);
 
 export const GET = handle(app);
 export const POST = handle(app);
